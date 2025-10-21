a2106fb84210e6192112974191493d3d53eb6af6
Kayode Bristol <3579196+kayodebristol@users.noreply.github.com>
2025-10-21 16:49:05 -0700

    Change render method to use Eta.render

--- src/tpl.ts
+++ src/tpl.ts
@@ -2,5 +2,5 @@
 import { Eta } from "eta";
 
 export function renderTemplate(tpl: string, data: unknown): string {
-  return Eta.renderString(tpl, data) as string;
+  return Eta.render(tpl, data) as string;
 }
