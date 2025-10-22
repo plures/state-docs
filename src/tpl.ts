14998bf5c887fd317cf87f9cc6a989ce6380561d
Kayode Bristol <3579196+kayodebristol@users.noreply.github.com>
2025-10-21 17:03:01 -0700

    Refactor renderTemplate to use Eta instance

--- src/tpl.ts
+++ src/tpl.ts
@@ -1,5 +1,6 @@
 import { Eta } from "eta";
 
 export function renderTemplate(tpl: string, data: unknown): string {
-  return Eta.render(tpl, data) as string;
+  const eta = new Eta();
+  return eta.renderString(tpl, data) as string;
 }
