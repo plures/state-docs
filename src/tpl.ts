3273e23c40cde34370dcdbd375c824e36d7fa35c
Kayode Bristol <3579196+kayodebristol@users.noreply.github.com>
2025-10-21 17:07:09 -0700

    Change data type from unknown to object in renderTemplate

--- src/tpl.ts
+++ src/tpl.ts
@@ -1,6 +1,6 @@
 import { Eta } from "eta";
 
-export function renderTemplate(tpl: string, data: unknown): string {
+export function renderTemplate(tpl: string, data: object): string {
   const eta = new Eta();
   return eta.renderString(tpl, data) as string;
 
